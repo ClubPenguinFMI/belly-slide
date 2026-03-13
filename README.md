323058a6cbb19fe8e1d1b994db6655cb79845add
Borimir Georgiev <imbobimobi@gmail.com>
2026-03-14 00:17:51 +0200

    project init

---
+++ README.md
@@ -0,0 +1,1 @@
+![Belly Sliding Penguin](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1aknudp9FsSjnCaTONPaYrFEH3qHFZo04Pw&s)
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShell.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nest-cli
+            pnpm
+          ];
+        };
+    };
+}
